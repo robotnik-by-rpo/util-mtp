6ab5f138b5ea02da4a06300cd63c3e234726bcf8
Monke <chikindin99@inbox.ru>
2026-08-03 17:01:56 +0000

    Added function valid googles' tokens

--- internal/google/net.go
+++ internal/google/net.go
@@ -17,17 +17,26 @@ type GoogleMetaData struct {
 }
 
 // Function for getting for Google
-func getTokken() *GoogleMetaData {
+func getToken() *GoogleMetaData {
 	return &GoogleMetaData{
 		accessToken: os.Getenv("GOOGLE_TOKEN"),
 		folder_id:   os.Getenv("GOOGLE_PATH_ID"),
 	}
 }
 
+func validToken(tokens *GoogleMetaData) bool{
+	return tokens.accessToken == "" && tokens.folder_id
+}
+
 // Function for sending to Google
-func SendToGoogleDisk(ctx *context.Context, filepath string) error {
+func SendToGoogleDisk(ctx context.Context, filepath string) error {
 	
-	tokens := getTokken()
+	tokens := getToken()
+	if validToken(tokens) {
+		log.Printf("Empty Google's tokens")
+		return fmt.Errorf("Empty tokens for Google")
+	}
+
 	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: tokens.accessToken})
 	driveService, err := drive.NewService(ctx, option.WithTokenSource(tokenSource))
 	if err != nil{
@@ -45,12 +54,12 @@ func SendToGoogleDisk(ctx *context.Context, filepath string) error {
 	fileInfo, err := file.Stat()
 	if err != nil{
 		log.Printf("Error getting inforamation about file: %v", err)
-		return fmt.Errorf("Error getting inforamation about file: %v", err)
+		return fmt.Errorf("Error getting information about file: %v", err)
 	}
 
-	driveFile := &drive.File{Name: file}
-	call := client.Files.Create(driveFile)
-	call = call.Media(file, googleapi.ChunkSize(0))
+	driveFile := &drive.File{Name: fileInfo.Name(), Parents: []string{tokens.folder_id}}
+	call := driveService.Files.Create(driveFile)
+	call = call.Media(file, googleapi.ChunkSize(googleapi.DefaultChunkSize))
 
 	res, err := call.Do()
 	if err != nil{
@@ -59,5 +68,5 @@ func SendToGoogleDisk(ctx *context.Context, filepath string) error {
 	}
 
 	fmt.Printf("File was uplouded successful %s", res.Id)
-
+	return nil
 }
